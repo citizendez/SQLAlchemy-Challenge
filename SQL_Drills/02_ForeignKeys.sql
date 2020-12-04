b9c072bbcb5ecab0adb6e6d039c94fb38ab6df85
Desiree Herschberger <citizendez@gmail.com>
2020-12-04 12:48:04 -0500

    drill 2

---
+++ SQL_Drills/02_ForeignKeys.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS departments;
+CREATE TABLE departments (
+  id INT NOT NULL,
+  dept_name VARCHAR(250) NOT NULL,
+  CONSTRAINT id_pkey PRIMARY KEY (id));
+
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees (
+  employee_id INT NOT NULL,
+  first_name VARCHAR(250) NOT NULL,
+  last_name VARCHAR(250) NOT NULL,
+  department_id INT NOT NULL,
+  CONSTRAINT employee_id_pkey PRIMARY KEY (employee_id),
+  CONSTRAINT department_id_fkey FOREIGN KEY (department_id) 
+        REFERENCES departments(id));
+		
