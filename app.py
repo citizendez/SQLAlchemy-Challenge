6d8a74c15072e197db944cd3f8d9c8690df85c15
Desiree Herschberger <citizendez@gmail.com>
2020-12-09 12:42:41 -0500

    update case

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import numpy as np
-import os.path
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 import pandas as pd
@@ -7,8 +6,6 @@ import json
 from flask import Flask, jsonify
 
 engine = create_engine(f"sqlite:///Resources/hawaii.sqlite")
-print(engine)
- 
 
 app = Flask(__name__)
 
@@ -33,22 +30,27 @@ def home():
 
 # additional routes
 
-@app.route('/api/v1.0/precipitation')
+@app.route('/api/v1.0/percipitation')
 def precipitation(): 
     sql = """
-        SELECT date, prcp FROM Measurement limit 10 
+        SELECT date, prcp FROM Measurement 
         """ 
     #exicutes a sql query
     results = pd.read_sql(sql, engine)
+    results.set_index('date', inplace=True)
+    df = results.transpose()
+    results_json = df.to_dict(orient='records')
+    return jsonify(results_json)
     #new list
-    lst = [ ]
-    #loop to change key to date with value perciptitation
-    for i, result in results.iterrows():
-        dic = { }
-        dic[result['date']] = result['prcp']
-        lst.append(dic)
+    #lst = [ ]
+    #this loop runs through the rows but takes a long time to run
+    # (loop to change key to date with value perciptitation)
+    #for i, result in results.iterrows():
+    #    dic = { }
+    #    dic[result['date']] = result['prcp']
+    #    lst.append(dic)
     #return new list of dictionarys
-    return  jsonify(lst)
+    #return  jsonify(lst)
 
 @app.route('/api/v1.0/stations')
 def stations(): 
@@ -88,8 +90,18 @@ def tobs():
     #returns dict as json
     return  jsonify(results_json)
 
+def is_date(date):
+    isdate = True
+    array = date.split('-')
+    if len(array) != 3:
+        isdate = False
+    return isdate
+
 @app.route('/api/v1.0/<start>')
-def start(start): 
+def start(start):
+    is_valid = is_date(start) 
+    if is_valid == False: 
+       return ('Invalid Date. Valid Dates format: 01-01-2017')   
     sql = f"""
 
 SELECT min(temp), max(temp), avg(temp)
@@ -97,6 +109,7 @@ FROM station_measurements
 WHERE date > '{start}';
 
     """
+    print(sql)
     #exicutes a sql query
     results = pd.read_sql(sql, engine)
     #converts reults to dictionary 
@@ -106,6 +119,8 @@ WHERE date > '{start}';
 
 @app.route('/api/v1.0/<start>/<end>')
 def start_end(start, end): 
+    if is_date(start) == False or is_date(end) == False: 
+       return ('Invalid Date. Valid Dates format: 01-01-2017') 
     sql = f"""
 
 SELECT min(temp), max(temp), avg(temp)
